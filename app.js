ea6abbb0eec6a57e69d26f47e8bcf7563a18b95c
danielkpodo <kpododanielnarh@gmail.com>
2020-04-13 14:31:20 +0000

    separated template into partials and added helmet security layer

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+
 const users = require("./routes/users");
 
+app.use(helmet());
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
